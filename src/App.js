3431b4778c98caadd6141803f0a4f4ffd7b8010b
Nawodawi <nawodawijebandara7@gmail.com>
2023-01-15 22:23:23 +0530

    fgjhg

--- src/App.js
+++ src/App.js
@@ -1,30 +1,70 @@
-import logo from './logo.svg';
+import { Button, Form, Input, Layout, Col, Row } from 'antd';
+import React from 'react';
 import './App.css';
+const { Header, Footer, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
 
-          Learn React
-          Hello there!
-
-          Explore World!
-
-        </a>
-      </header>
-    </div>
-  );
+const MyFormItemContext = React.createContext([]);
+function toArr(str) {
+  return Array.isArray(str) ? str : [str];
 }
+const MyFormItemGroup = ({ prefix, children }) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
+  return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
+};
+const MyFormItem = ({ name, ...props }) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
+  return <Form.Item name={concatName} {...props} />;
+};
+
+const App = () => {
+  const onFinish = (value) => {
+    console.log(value);
+  };
+  return (
+
+    <Layout>
+
+      <Header>DevOps Hands Onnnn</Header>
+      <Content>
+      
+      <Row>
+      <Col span={8}></Col>
+      <Col span={8} className="space-align-blockd">
+      
+        <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
+          <MyFormItemGroup prefix={['user']}>
+            <MyFormItemGroup prefix={['name']}>
+              <MyFormItem name="firstName" label="First Name">
+                <Input />
+              </MyFormItem>
+              <MyFormItem name="lastName" label="Last Name">
+                <Input />
+              </MyFormItem>
+            </MyFormItemGroup>
+
+            <MyFormItem name="age" label="Age">
+              <Input />
+            </MyFormItem>
+          </MyFormItemGroup>
+
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form>
+        </Col>
+        <Col span={8}></Col>
+    </Row>
+
+      </Content>
+      <Footer></Footer>
+
+    </Layout>
+
+  );
+};
 
 export default App;
+
